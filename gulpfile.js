8a14951193f42cfe8931163a5845d4f512c6b11e
viniciusllima <viniciusllima@gmail.com>
2018-03-26 14:09:01 -0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp            = require('gulp');
+var sass            = require('gulp-sass');
+var browserSync     = require('browser-sync');
+var uglify          = require('gulp-uglify');
+var plumber         = require('gulp-plumber');
+var rename          = require("gulp-rename");
+var imagemin        = require("gulp-imagemin");
+var pngquant        = require('imagemin-pngquant');
+var concat          = require('gulp-concat');
+var merge           = require('merge-stream');
+var babel           = require('gulp-babel');
+var browserify      = require('gulp-browserify');
+
+gulp.task('browser-sync', function() {
+  browserSync.init(['src/assets/css/*.css', 'src/assets/js/**/*.js', 'index.html'], {
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+
+var config = {
+    dest: 'assets/'
+}
+
+gulp.task('js', function() {
+  var jsFiles = [
+    'src/assets/js/app.js'
+  ];
+
+  gulp.src(jsFiles)
+  .pipe(babel({presets: ['es2015']}))
+  .pipe(concat('app.js'))
+  .pipe(browserify({
+    insertGlobals : true
+  }))  
+  .pipe(uglify())
+  .pipe(gulp.dest(config.dest + 'js'))
+});
+
+gulp.task('sass', function() {
+  gulp.src('src/assets/sass/**/*.scss')
+  //.pipe(sass({outputStyle: 'compressed'}))
+  .pipe(sass())
+  .pipe(plumber())
+  .pipe(gulp.dest(config.dest + 'css'));
+});
+
+// TODO
+gulp.task('images', function () {
+  return gulp.src('src/assets/images/*')
+  .pipe(imagemin({
+    progressive: true,
+    svgoPlugins: [{removeViewBox: false}],
+    use: [pngquant()]
+  }))
+  .pipe(gulp.dest('src/assets/images'));
+});
+
+gulp.task('default', ['browser-sync', 'sass', 'js', 'images'], function() {
+  gulp.watch('src/assets/sass/**/*.scss', ['sass']);
+  gulp.watch('src/assets/js/**/*.js', ['js']);
+});
---
+++ src/assets/js/app.js
@@ -0,0 +1,33 @@
+(function () {
+  // -- Vanilla JS
+
+  require('./test');
+  const $ = require('jquery');
+   
+  // https://www.raphaelfabeni.com.br/rv/hotels.json
+
+  
+  const splitHotel = (dataObj, priceInit, priceEnd, rate) => {
+    $(dataObj.hotels).each(function(index) {
+      console.log(priceInit, priceEnd, dataObj.hotels[index].price) ;
+      if((dataObj.hotels[index].price <= priceEnd && dataObj.hotels[index].price >= priceInit) && dataObj.hotels[index].rate === rate) {
+        console.log(dataObj.hotels[index]);
+      }
+    });
+  }
+
+  $.ajax({
+    url : 'https://www.raphaelfabeni.com.br/rv/hotels.json',
+    type : 'GET',
+    success : function(data) {      
+      splitHotel(data, 200, 900, 3);
+    },
+    error : function(jqXHR, textStatus) {
+      console.log(jqXHR);
+    }
+  })
+
+  
+  
+
+}());
