29dff3b19b08b624059371ff501bb0ae704a5c04
Vinicius Lima <viniciusllima@gmail.com>
2018-03-26 21:46:27 -0300

    Add new comparison

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -9,7 +9,7 @@
   
   const splitHotel = (dataObj, priceInit, priceEnd, rate) => {
     $(dataObj.hotels).each(function(index) {
-      console.log(priceInit, priceEnd, dataObj.hotels[index].price) ;
+      console.log(priceInit, priceEnd, dataObj.hotels[index].price, dataObj.hotels[index].rate) ;
       if((dataObj.hotels[index].price <= priceEnd && dataObj.hotels[index].price >= priceInit) && dataObj.hotels[index].rate === rate) {
         console.log(dataObj.hotels[index]);
       }
@@ -20,7 +20,7 @@
     url : 'https://www.raphaelfabeni.com.br/rv/hotels.json',
     type : 'GET',
     success : function(data) {      
-      splitHotel(data, 200, 900, 3);
+      splitHotel(data, 100, 900, 3);
     },
     error : function(jqXHR, textStatus) {
       console.log(jqXHR);
